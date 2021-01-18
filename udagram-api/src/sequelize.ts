617d5a52b2015e112a28b0004a0937fa927ee310
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-01-18 11:58:57 +0100

    preparing project to run locally

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,15 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
+console.log("Configuration before creating sequelize object");
+console.log(JSON.stringify(config));
 
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
   'dialect': config.dialect,
   'storage': ':memory:',
 });
+
--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -4,15 +4,17 @@ import {sequelize} from './sequelize';
 
 import {IndexRouter} from './controllers/v0/index.router';
 
-import bodyParser from 'body-parser';
 import {config} from './config/config';
+import bodyParser from 'body-parser';
 import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+
 
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
+
 
   const app = express();
   const port = process.env.PORT || 8080;
