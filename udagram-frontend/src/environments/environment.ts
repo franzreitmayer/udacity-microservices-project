a2030e39f10adb3eb24d8fcf0aa6b03a5be91dc3
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-02-01 01:05:59 +0100

    api host at environment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-reverse-proxy:8080/api/v0'
+  apiHost: 'http://ab557fa7e3763405081dddc74e51fe8a-841789916.eu-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
